c2e8de7b270cfcdf97c077c14f6ccb560a7fbc1e
leonzh2k <leonzh2k@gmail.com>
2020-10-18 22:58:46 -0700

    update readme, update timerSetup

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
 # PyProgramTimer
-A simple script that when executed generates files to help you test and compare runtimes of your programs.
+A simple script that when executed generates files to help you test and compare runtimes of your Python programs.
 
-Requirements:
-Python 3.7.3 or above. 
+## Introduction
+### Background and Purpose
+There was a period of time where I was really into solving Euler Project problems. Oftentimes, the programs I wrote to solve the problems would have really long runtimes, since unless there was some mathematical trick that could be used, an obscene amount of mathematical operations would be required to derive the answer. However, it is written on the website that with an efficient implementation, answers to problems can be derived in under *one minute*. Needless to say, my programs were not meeting that standard. Thus, I became curious about seeing the runtimes of my solutions and how they got better with successive iterations. And thus the idea for this script was born.
 
-Instructions: Create an empty folder that you wish to store your program in. Drag timerSetup.py into the folder, and execute it ONLY ONCE. Delete timerSetup.py. Build your program in "name.py."
+## Requirements
+This script was written in Python 3.7.3, but may work on older versions.
+
+## Setup
+
+1. Download `timerSetup.py`. 
+2. Create an empty directory that you wish to develop your program in. 
+3. Move `timerSetup.py` into the directory, and execute it ONLY ONCE. 
+4. You will see two new files, `numruns.txt` and `runtimes.txt`. `runtimes.txt` keeps track of runtimes, and `numruns.txt` is used to keep track of how many runs have been logged to `runtimes.txt`.
+4. Delete or move `timerSetup.py` out of the directory (you can do this anytime). 
+
+
+## Usage
+The general use cycle of this script would probably be something like this:
+1. Write the first (working) iteration of your program.
+2. Record the runtime on `runtimes.txt`.
+3. Make some changes to your program and run.
+4. Record the runtime on `runtimes.txt`.
+5. Repeat steps 3-4.
+6. See how much faster your program has gotten and how far you've come.....
--- timerSetup.py
+++ timerSetup.py
@@ -1,55 +1,129 @@
+import os.path
+
 """
 Script to automatically generate a .py file with all time testing material included
-as well as two text documents, one containing the runtimes, one keeping track of the
-number of runs. RUN ONLY ONCE. THIS MUST BE PLACED IN THE SAME FILE AS THE PROGRAM YOU WANT TO TEST.
+as well as two text documents, one containing the runtime data, one keeping track of the
+number of runs. RUN ONLY ONCE. THIS MUST BE PLACED IN THE SAME DIRECTORY AS THE PROGRAM YOU WANT TO TEST.
 """
 
-with open('name.py', 'w') as file1:
-    file1.write(
+def createRunTimes():
+    with open('runtimes.txt', 'w') as file2:
+        file2.write('')
+    
+def createNumRuns():
+    with open('numruns.txt', 'w') as file3:
+        file3.write('0') #0 runs yet
+
+def createNamePy():
+    with open('name.py', 'w') as file1:
+        """
+        The indentation of the write string may look weird, 
+        but it's intended. Don't change it unless you know 
+        what you're doing or it may mess up the indentation 
+        in name.py and you'll have to manually fix it.
+        """
+        file1.write(
 '''import time
 from datetime import datetime
 timer = time.time()
+#######################################
 
 #WRITE YOUR PROGRAM HERE
 
+#######################################
 now = datetime.now()
-current_time = now.strftime("(%d/%m/%Y %H:%M:%S)\\n")
+current_date = now.strftime("(%m/%d/%Y %I:%M:%S %p)\\n")
 runtime = time.time() - timer
-time_secs = "finished in" + " " + str(runtime) + " " + "seconds" + "\\n"
-time_mins = "or" + " " + str(runtime / 60) + " " + "minutes" + "\\n"
-time_hrs =  "or" + " " + str(runtime / 3600) + " " + "hours" + "\\n"
+time_secs = str(runtime) + " " + "seconds" + "\\n"
+time_mins = str(runtime / 60) + " " + "minutes" + "\\n"
+time_hrs =  str(runtime / 3600) + " " + "hours" + "\\n"
+print("Finished in:")
+print("====================")
 print(time_secs, end='')
+print("or")
 print(time_mins, end='')
+print("or")
 print(time_hrs, end='')
 
         
-decision = str(input("Would you like to write this time to the list of runtimes? (y/n)\\n"))
+decision = str(input("Would you like to write this time to the list of runtimes? (y/n): "))
+while (decision != "y" and decision != "n"):
+    decision = str(input("Would you like to write this time to the list of runtimes? (y/n): "))
 if decision == "y":
-    with open("metadata.txt", "r+") as metadata:
-        for line in metadata:
+    with open("numruns.txt", "r+") as numruns:
+        for line in numruns:
             num_runs = int(line[0])
             num_runs += 1
-            current_time = "Run " + str(num_runs) + " " + current_time
-            metadata.seek(0)
-            metadata.truncate(0)
-            metadata.write(str(num_runs))
+            current_date = "Run " + str(num_runs) + " " + current_date
+            numruns.seek(0)
+            numruns.truncate(0)
+            numruns.write(str(num_runs))
             
     with open("runtimes.txt", "a") as runtimes:
-        runtimes.write("\\n")
-        runtimes.write(current_time)
+        runtimes.write(current_date)
+        runtimes.write("------------------------------\\n")
         runtimes.write(time_secs)
+        runtimes.write("or\\n")
         runtimes.write(time_mins)
+        runtimes.write("or\\n")
         runtimes.write(time_hrs)
+        runtimes.write("\\n")
+        runtimes.write("==============================\\n\\n")
 '''
 )
 
-    
-    
-with open('runtimes.txt', 'w') as file2:
-    file2.write('')
+def main():
+    #os.path.isfile checks whether the name.py file already exists
+    if os.path.isfile('name.py'):
+        overwriteName = str(input("A file called 'name.py' already exists in this directory. Are you sure you want to overwrite it? (y/n): "))
+        while (overwriteName != "y" and overwriteName != "n"):
+            overwriteName = str(input("A file called 'name.py' already exists in this directory. Are you sure you want to overwrite it? (y/n): "))
+        if overwriteName == "y":
+            createNamePy()
+            print("name.py successfully overwritten.\n")
+        else:
+            pass
+    else:
+        createNamePy()
+        print("Successfully created name.py.\n")
 
-with open('metadata.txt', 'w') as file3:
-    file3.write('0') #0 runs yet
+    if os.path.isfile('runtimes.txt'):
+        overwriteRunTimes = str(input("A file called 'runtimes.txt' already exists in this directory. Are you sure you want to overwrite it? (y/n): "))
+        while (overwriteRunTimes != "y" and overwriteRunTimes != "n"):
+            overwriteRunTimes = str(input("A file called 'runtimes' already exists in this directory. Are you sure you want to overwrite it? (y/n): "))
+        if overwriteRunTimes == "y":
+            createRunTimes()
+            print("runtimes.txt successfully overwritten.\n")
+        else:
+            pass
+    else:
+        createRunTimes()
+        print("Successfully created runtimes.txt.\n")
+
+    if os.path.isfile('numruns.txt'):
+        overwriteNumRuns = str(input("A file called 'numruns.txt' already exists in this directory. Are you sure you want to overwrite it? (y/n): "))
+        while (overwriteNumRuns != "y" and overwriteNumRuns != "n"):
+            overwriteNumRuns = str(input("A file called 'numruns.txt' already exists in this directory. Are you sure you want to overwrite it? (y/n): "))
+        if overwriteNumRuns == "y":
+            createNumRuns()
+            print("numruns.txt successfully overwritten.")
+        else:
+            pass
+    else:
+        createNumRuns()
+        print("Successfully created numruns.txt.")
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
 
 
 
